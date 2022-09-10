f82c3f0b7be3c1151a5aa1d715130e8310a32028
andrew <andrewnisttahuz@gmail.com>
2022-09-10 11:58:56 +0200

    joi

---
+++ validators/Schemas.js
@@ -0,0 +1,39 @@
+const Joi = require("joi"); 
+app.post("/register", async (req, res) => {
+
+    try {
+
+    // Definimos schema y comprobamos con postman
+    const schema = Joi.object({
+        username: Joi.string().min(6).alphanum().uppercase().required(),   
+        password:Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+        confirm_password:Joi.string().equal(Joi.ref('password')).messages({'any.only': 'password does not match' }).required(),
+        firstname: Joi.string().required(),
+        lastname: Joi.string(),
+        email: Joi.string().email({minDomainSegments: 2}).required(),
+
+    });
+
+    // Validate req.body against the defined schema
+    const validation = schema.validate(req.body);
+    const { value, error } = validation;
+
+    if (error) {
+        const message = error.details.map(x => x.message);
+    
+        res.status(400).json({
+            status: "error",
+            message: "Invalid request data",
+            data: message
+    });
+        } else {
+        res.json({
+            status: "success",
+            message: "Registration successful",
+            data: value
+    });
+        }
+        } catch (error) {
+        res.json({status:"failed",message:error.message})
+    }
+    });
